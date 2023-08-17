0a25346615416e457b452a80724dde94072a2c1b
Nidz-Web3 <106298826+Nidz-Web3@users.noreply.github.com>
2023-08-17 21:38:26 +0700

    Create EmojiPicker.vue
    Develop, add emojis

---
+++ components/EmojiPicker.vue
@@ -0,0 +1,49 @@
+<template>
+  <button class="btn btn-outline-primary me-2 mt-2 "
+   @click="toggle">
+   <i class="bi bi-emoji-smile"></i>
+   EMO</button>
+   <Picker
+    v-if="emojiPickerSelected"
+    :data="emojiIndex"
+    set="apple" 
+    title="Pick your emoji…"
+    emoji="point_up"
+    @select="convertEmoji"
+    class="emoji-mart-category"
+    :style="{ backgroundColor: '#222529', color: '#FFFFFF',  }" 
+  />
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Picker, EmojiIndex } from 'emoji-mart-vue-fast/src'
+import data from 'emoji-mart-vue-fast/data/all.json'
+
+export default {
+  name: 'EmojiPicker',
+  components: {
+    Picker,
+  },
+  emits: ['updateEmoji'],
+  setup(props, context) {
+    const emojiPickerSelected = ref(false)
+    let emojiIndex = new EmojiIndex(data)
+
+    const toggle = () => {
+      emojiPickerSelected.value = !emojiPickerSelected.value
+    }
+
+    const convertEmoji = (emoji) => {
+      context.emit('updateEmoji', emoji.native)
+    }
+
+    return {
+      emojiPickerSelected,
+      emojiIndex,
+      toggle,
+      convertEmoji,
+    }
+  },
+}
+</script>
