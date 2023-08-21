36c10088f8ab7c500c11fcfa8c255a95b69bed68
Nidz The Fact <106298826+Nidz-Web3@users.noreply.github.com>
2023-08-21 16:05:41 +0700

    Update EmojiPicker.vue
    Edit

--- components/EmojiPicker.vue
+++ components/EmojiPicker.vue
@@ -1,5 +1,6 @@
 <template>
-  <button data-bs-toggle="modal" data-bs-target="#emojiModal" class="btn btn-outline-primary me-2 mt-2">
+  <button data-bs-target="#emojiModal" class="btn btn-outline-primary me-2 mt-2"
+   @click="toggleEmojiPicker" >
     <i class="bi bi-emoji-smile"></i>
   </button>
 
@@ -8,14 +9,14 @@
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="emojiModalLabel">Pick an Emoji</h1>
-          <button id="closeEmojiModal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button id="closeEmojiModal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                  @click="closeEmojiPicker"></button>
         </div>
         <div class="modal-body">
-          <Picker
+          <EmojiMartPicker
+            v-if="emojiPickerSelected"
             :data="emojiIndex"
             set="apple"
-            title="Pick an emoji"
-            emoji="point_up"
             @select="handleEmojiSelect"
             class="emoji-mart-category"
             :style="{ backgroundColor: '#222529', color: '#FFFFFF' }"
@@ -27,27 +28,54 @@
 </template>
 
 <script>
-import { Picker, EmojiIndex } from 'emoji-mart-vue-fast/src'
-import data from 'emoji-mart-vue-fast/data/apple.json'
+import { ref } from 'vue'
+import { EmojiIndex } from 'emoji-mart-vue-fast/src/utils/emoji-data'
+import EmojiMartPicker from 'emoji-mart-vue-fast/src/components/Picker'
+
+import data from 'emoji-mart-vue-fast/data/all.json'
 
 export default {
   name: 'EmojiPickerWithModal',
   components: {
-    Picker,
+    EmojiMartPicker,
   },
   emits: ['updateEmoji'],
 
   setup(props, context) {
+    const emojiPickerSelected = ref(false)
     const emojiIndex = new EmojiIndex(data)
+    let modalInstance = null;
+
+    const toggleEmojiPicker = () => {
+      emojiPickerSelected.value = !emojiPickerSelected.value;
+
+      if (emojiPickerSelected.value) {
+        modalInstance = new bootstrap.Modal(document.getElementById('emojiModal'));
+        modalInstance.show();
+      } else if (modalInstance !== null && modalInstance._isShown) {
+        modalInstance.hide();
+      }
+    }
+
+    const closeEmojiPicker = () => {
+      emojiPickerSelected.value = false;
+
+      if (modalInstance !== null && modalInstance._isShown) {
+        modalInstance.hide();
+      }
+    }
 
     const handleEmojiSelect = (emoji) => {
-      document.getElementById('closeEmojiModal').click();
       context.emit('updateEmoji', emoji.native);
+      closeEmojiPicker();
     }
 
     return {
+      emojiPickerSelected,
       emojiIndex,
-      handleEmojiSelect
+      toggleEmojiPicker,
+      handleEmojiSelect,
+      closeEmojiPicker,
     }
   },
 }
